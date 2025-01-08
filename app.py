e872552f0735797cece628df3ec674f6a51f32e4
AkashR-16 <99939852+AkashR-16@users.noreply.github.com>
2025-01-08 20:57:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,71 @@
+import streamlit as st
+import os
+from langchain_groq import ChatGroq
+from langchain_ollama import OllamaEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+
+from dotenv import load_dotenv
+load_dotenv()
+
+# Load API keys
+groq_api_key = os.getenv('GROQ_API_KEY')
+
+# Streamlit app title
+st.title('ChatGroq with Llama3')
+
+# Initialize the ChatGroq LLM
+llm = ChatGroq(groq_api_key=groq_api_key, model_name="Llama3-8b-8192")
+
+# Prompt template
+prompt = ChatPromptTemplate.from_template(
+    """
+    Answer the questions based on the provided context only.
+    Please provide the most accurate response based on the question.
+    <context>
+    {context}
+    <context>
+    Question: {input}
+    """
+)
+
+# Function to create vector embeddings
+def vector_embeddings():
+    if "vectors" not in st.session_state:
+        st.session_state.embeddings = OllamaEmbeddings(model="llama3.1")
+        st.session_state.loader = PyPDFDirectoryLoader("./us_census")  # Data ingestion
+        st.session_state.docs = st.session_state.loader.load()  # Document loading
+        st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)  # Chunk creation
+        st.session_state.final_documents = st.session_state.text_splitter.split_documents(st.session_state.docs)  # Splitting
+        st.session_state.vectors = FAISS.from_documents(st.session_state.final_documents, st.session_state.embeddings)  # Vector store
+
+# Streamlit input
+prompt1 = st.text_input("Enter your question from Documents!!")
+
+# Button to start vector embedding
+if st.button("Document Embeddings"):
+    vector_embeddings()
+    st.write("Vector Store DB is Ready!!")
+
+# Document retrieval and response generation
+if prompt1:
+    document_chain = create_stuff_documents_chain(llm, prompt)
+    retriever = st.session_state.vectors.as_retriever()
+    retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+    # Measure response time
+    import time
+    start = time.process_time()
+    response = retrieval_chain.invoke({'input': prompt1})
+    st.write(f"Response time: {time.process_time() - start:.2f} seconds")
+    st.write(response['answer'])
+
+    # Display document similarity search results
+    with st.expander("Document similarity search"):
+        for i, doc in enumerate(response["context"]):
+            st.write(doc.page_content)
+            st.write("--------------------------------")
